efae049717f47982bf44d25779400a47ab12a0df
gopikrishna000 <76482198+gopikrishna000@users.noreply.github.com>
2022-11-18 10:50:16 +0530

    Create crt.cpp

---
+++ crt.cpp
@@ -0,0 +1,47 @@
+	
+int extended_euclidean(int a, int b, int *x, int *y)
+{
+	if (a == 0) {
+		*x = 0; *y = 1;
+		return b;
+	}
+	int x1, y1;
+	int gcd = extended_euclidean(b % a, a, &x1, &y1);
+	*x = y1 - (b / a) * x1;
+	*y = x1;
+	return gcd;
+}
+
+int inverse(int a, int m) {
+	int x, y;
+	int g = extended_euclidean(a, m, &x, &y);
+	if (g != 1) {
+		return -1;
+	}
+	else {
+		x = (x % m + m) % m;
+		return x;
+	}
+}
+
+//(residue[],modulus[],modulus product) 
+int crt(vi a, vi m, int M) {
+	int n = a.size();
+	vi c(n); vi Mi(n);
+	for (int i = 0; i < n; i++) {
+		Mi[i] = M / m[i];
+	}
+
+	for (int i = 0; i < n; i++) {
+		c[i] = Mi[i] * (inverse(Mi[i], m[i]));
+	}
+
+	int x = 0;
+	for (int i = 0; i < n; ++i) {
+		x += a[i] * c[i];
+		x %= M;
+	}
+
+	return x;
+}
+
